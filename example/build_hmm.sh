d9d7b3f4316dbd6fc3dfd47d35f3002cda1d0fb9
YanCCscu <yccvican@gmail.com>
2020-04-28 13:07:53 +0800

    update example files

--- example/build_hmm.sh
+++ example/build_hmm.sh
@@ -1,4 +1,6 @@
 #http://www.gsp.com/cgi-bin/man.cgi?topic=hmmbuild
+#the fasta files were aligned with software such as clustalw2
+#a example like: clustalw2 -INFILE=Tyr.fas -ALIGN -OUTFILE=Tyr.aln
 #usage: sh build_hmm.sh aligned_fasta/*
 for f in $@
 do
